ae579804fa11d74906cb2703389d3cbf7c61b194
Iryna Tiselko <ms.itiselko@gmail.com>
2023-08-21 22:51:03 +0300

    clw 24

---
+++ students/tiselko_iryna/class_works/24/src/js/script.js
@@ -0,0 +1,159 @@
+/*const userNum1 = +prompt('Enter your number');
+
+if (userNum1 > 0) {
+    console.log('+');
+} else if (userNum1 < 0) {
+    console.log('-');
+} else {
+    console.log('0');
+}*/
+
+
+/*const userMonth = +prompt('Enter your number from 1 to 12');
+switch (userMonth) {
+    case 1:
+        console.log('Січен');
+        break;
+    case 2:
+        console.log('Лютий');
+        break;
+    case 3:
+        console.log('Березень');
+        break;
+    case 4:
+        console.log('Квітень');
+        break;
+    case 5:
+        console.log('Травень ');
+        break;
+    case 6:
+        console.log('Червень');
+        break;
+    case 7:
+        console.log('Липень');
+        break;
+    case 8:
+        console.log('Серпень');
+        break;
+    case 9:
+        console.log('Вересень');
+        break;
+    case 10:
+        console.log('Жовтень');
+        break;
+    case 11:
+        console.log('Листопад');
+        break;
+    case 12:
+        console.log('Грудень');
+        break;
+    default:
+        console.log('Wrong Number');
+}*/
+
+/*const userMonth = +prompt('Enter your number from 1 to 12');
+switch (userMonth) {
+    case 1:
+        console.log('Січен');
+    case 2:
+        console.log('Лютий');
+    case 3:
+        console.log('Березень');
+    case 4:
+        console.log('Квітень');
+    case 5:
+        console.log('Травень ');
+    case 6:
+        console.log('Червень');
+    case 7:
+        console.log('Липень');
+    case 8:
+        console.log('Серпень');
+    case 9:
+        console.log('Вересень');
+    case 10:
+        console.log('Жовтень');
+    case 11:
+        console.log('Листопад');
+    case 12:
+        console.log('Грудень');
+        break;
+    default:
+        console.log('Wrong Number');
+}*/
+
+
+
+/*const userVar = prompt('Choose yes or no');
+let isAccsess;
+switch (userVar?.toLowerCase()) {
+    case 'yes':
+        isAccsess = true;
+        break;
+    case 'no':
+        isAccsess = false;
+        break;
+    default:
+        console.log('Wrong answer');
+}
+console.log(isAccsess);*/
+
+/*const userVar = prompt('Choose yes or no');
+let isAccsess = userVar === 'yes' ? '+' : userVar === 'no' ? '-' : undefined;
+console.log(isAccsess);*/
+
+/*let i = 0;
+const n = 10;
+while (n > i) {
+    console.log(i);
+    i++;
+}*/
+
+/*const userVar = +prompt('Enter your number');
+let i = 0;
+let message = '';
+
+while (userVar > i) {
+    message = message + '#';
+    i++;
+}
+console.log(message);*/
+
+
+/*let myAge;
+do {
+    myAge = +prompt('What is your age?');
+} while (!myAge || myAge < 0 || myAge > 130);
+console.log(myAge);*/
+
+/*let yourVariant;
+do {
+    yourVariant = +prompt('Resolve next example: 2 +2 * 2?');
+} while (yourVariant !== 6);
+console.log(yourVariant);*/
+
+/*const n = 7;
+for (let i = 0; n > i; i++) {
+    console.log(i);
+}*/
+
+
+/*const stringText = 'What is your age?';
+console.log(stringText.length, stringText[0], stringText[stringText.length - 1]);
+for (let i = 0; stringText.length > i; i++) {
+    console.log(stringText[i]);
+}*/
+
+let m = '';
+n = 10;
+const countCol = 10;
+for (let i = 0; n > i; i++) {
+    m += (i + 1) + '. ';
+    for (let j = 0; j < countCol; j++) {
+        m += '#'
+    }
+    m += '\n'
+}
+console.log(m);
+
+
