27c741beb5e072e96ed490b89bde790d89be7ece
Iryna Tiselko <ms.itiselko@gmail.com>
2023-10-09 22:29:05 +0300

    create clw 41_React

---
+++ students/tiselko_iryna/class_works/41_react/src/js/App.js
@@ -0,0 +1,11 @@
+import { createElement } from "react";
+import Advice from "./components/Advice.js";
+
+function App() {
+    return createElement(
+        Advice
+    );
+}
+
+
+export default App;
