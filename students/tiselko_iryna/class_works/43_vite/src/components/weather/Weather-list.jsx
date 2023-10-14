f72bab85b2b509535760f671afdbe0649a6cac90
Iryna Tiselko <ms.itiselko@gmail.com>
2023-10-14 19:51:25 +0300

    clw 43 first  part

---
+++ students/tiselko_iryna/class_works/43_vite/src/components/weather/Weather-list.jsx
@@ -0,0 +1,20 @@
+import Weather from "./Weather";
+
+export default function WeatherList() {
+
+    return (
+        <div className="row weather__row">
+            <Weather colClass="col-6" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-3" city="Paris" temp={11} iconId="paris" />
+            <Weather colClass="col-3" city="Belgrad" temp={21} iconId="belgrade" />
+            <Weather colClass="col-3" city="Venice" temp={22} iconId="venice" />
+            <Weather colClass="col-3" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-3" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-3" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-3" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-6" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-3" city="Lisbon" temp={21} iconId="lisbon" />
+            <Weather colClass="col-12" city="Lisbon" temp={21} iconId="lisbon" />
+        </div>
+    );
+}
